9711a7172a15bd268246431a788d11cc7e369b92
PremChandu-Pusthala <premchandu.pusthela003@gmail.com>
2025-05-15 20:54:39 +0530

    Comparing numbers by taking both numbers as input

---
+++ Number_comparision.py
@@ -0,0 +1,9 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+if first_number > second_number:
+    print("First number is Greater.")
+elif first_number < second_number:
+    print("Second number is Greater.")
+else:
+    print("Both numbers are equal.")
