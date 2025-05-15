249a7847a5e1892b5001fe7506fd39d520cf26f2
PremChandu-Pusthala <premchandu.pusthela003@gmail.com>
2025-05-15 17:20:27 +0530

    The code that contains the comments that explains the different parts of the code

---
+++ Explaining_with_comments.py
@@ -0,0 +1,4 @@
+# This script prints a personal greeting
+name = "Prem Chandu"  # Store the user's name in a variable
+message = "Welcome to TSX-Unstop-Python Internship" #The actual greeting message
+print("Hello, " + name + "! "+  message)  # Print greeting
